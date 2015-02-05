3c031ba8d06b19f226520651da5b70300c8b022c
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-02-05 06:43:27 -0800

    Pass 3rd and final test case

--- src/challenge200/Solution.java
+++ src/challenge200/Solution.java
@@ -30,36 +30,40 @@ public class Solution {
         String inputFileName = scanner.nextLine();
 
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("challenge200/" + inputFileName));
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("input-files/challenge200/" + inputFileName));
             List<String[]> grid = buildGrid(bufferedReader);
             List<String> floodFillStartingInfo = Arrays.asList(bufferedReader.readLine().split(" "));
             floodFillGrid(grid, Integer.parseInt(floodFillStartingInfo.get(1)), Integer.parseInt(floodFillStartingInfo.get(0)), floodFillStartingInfo.get(2));
-            grid.forEach((String[] row) -> System.out.println( Arrays.deepToString(row)));
+            grid.forEach((String[] row) -> System.out.println(Arrays.deepToString(row)));
 
         } catch (Exception exception) {
             System.out.println(exception.getCause().toString());
         }
     }
 
-    private static void floodFillGrid(List<String[]> grid, Integer xCoordinate, Integer yCoordinate, String newCharacter) {
-        String currentCharacter = grid.get(xCoordinate)[yCoordinate];
-        if (borderCharacters.contains(currentCharacter) || currentCharacter.equals(newCharacter)) {
+    private static void floodFillGrid(List<String[]> grid, Integer yCoordinate, Integer xCoordinate, String newCharacter) {
+        if (xCoordinate < 0 || yCoordinate < 0 || yCoordinate >= grid.size() || xCoordinate >= grid.get(yCoordinate).length) {
             return;
         } else {
-            if (currentCharacter.equals(targetCharacter)) {
-                grid.get(xCoordinate)[yCoordinate] = newCharacter;
+            String currentCharacter = grid.get(yCoordinate)[xCoordinate];
+            if (borderCharacters.contains(currentCharacter) || currentCharacter.equals(newCharacter)) {
+                return;
+            } else {
+                if (currentCharacter.equals(targetCharacter)) {
+                    grid.get(yCoordinate)[xCoordinate] = newCharacter;
+                }
+                floodFillGrid(grid, yCoordinate + 1, xCoordinate, newCharacter);
+                floodFillGrid(grid, yCoordinate - 1, xCoordinate, newCharacter);
+                floodFillGrid(grid, yCoordinate, xCoordinate + 1, newCharacter);
+                floodFillGrid(grid, yCoordinate, xCoordinate - 1, newCharacter);
             }
-            floodFillGrid(grid, xCoordinate+1, yCoordinate, newCharacter);
-            floodFillGrid(grid, xCoordinate-1, yCoordinate, newCharacter);
-            floodFillGrid(grid, xCoordinate, yCoordinate+1, newCharacter);
-            floodFillGrid(grid, xCoordinate, yCoordinate-1, newCharacter);
         }
     }
 
     private static List<String[]> buildGrid(BufferedReader bufferedReader) throws IOException {
         String[] dimensions = bufferedReader.readLine().split(" ");
         String[][] grid = new String[Integer.parseInt(dimensions[1])][Integer.parseInt(dimensions[0])];
-        for (int current = 0; current < grid.length; current++){
+        for (int current = 0; current < grid.length; current++) {
             grid[current] = bufferedReader.readLine().split("");
         }
         return Arrays.asList(grid);
