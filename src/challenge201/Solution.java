93c99c855a5fc48e7d649ea6a521d26fd6acc3a5
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-02-09 20:42:12 -0800

    Solution for easy challenge 201 from /r/dailyprogrammer, uses classes from Java 8's java.time.

---
+++ src/challenge201/Solution.java
@@ -0,0 +1,23 @@
+package challenge201;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
+/**
+ * Created by jon on 2/9/15.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.print("Please enter a date with the format yyyy-MM-dd: ");
+        Scanner scanner = new Scanner(System.in);
+        String dateString = scanner.nextLine();
+        String[] dateArray = dateString.split("-");
+        LocalDate targetDate = LocalDate.of(Integer.parseInt(dateArray[0]), Integer.parseInt(dateArray[1]), Integer.parseInt(dateArray[2]));
+        LocalDate today = LocalDate.now();
+        Long numberOfDaysBetween = ChronoUnit.DAYS.between(today, targetDate);
+        System.out.println(numberOfDaysBetween + " days from " + today.toString() + " to " + targetDate.toString());
+    }
+}
