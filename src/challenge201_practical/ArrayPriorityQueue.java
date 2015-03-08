d9b1237edf59a398d22ca3a30260030ef68413d7
jonathan <jonathan.wilfredo.fuentes@gmail.com>
2015-03-07 17:26:31 -0800

    Finished array-based priority queue.

--- src/challenge201_practical/ArrayPriorityQueue.java
+++ src/challenge201_practical/ArrayPriorityQueue.java
@@ -1,6 +1,7 @@
 package challenge201_practical;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * Created by jon on 2/16/15.
@@ -17,16 +18,53 @@ public class ArrayPriorityQueue<E extends Comparable<E>> {
 
     public boolean insert(E element) {
         maxHeapArray[nextOpenIndex] = element;
+        percolateUp(nextOpenIndex);
         nextOpenIndex++;
-        return (maxHeapArray[nextOpenIndex -1] != null);
+        return (maxHeapArray[nextOpenIndex - 1] != null);
     }
 
-    private void percolateUp(E element, Integer currentIndex) {
+    public E dequeue() throws PriorityQueueEmptyException {
+        if (count() == 0) {
+            throw new PriorityQueueEmptyException();
+        }
+        E rootElement = maxHeapArray[0];
+        if (count() > 1) {
+            int lastIndexUsed = nextOpenIndex - 1;
+            percolateDown(lastIndexUsed);
+        }
+        return rootElement;
+    }
+
+    private void percolateDown(Integer currentIndex) {
+        maxHeapArray[0] = maxHeapArray[currentIndex];
+        E currentElement = maxHeapArray[currentIndex];
+        maxHeapArray[currentIndex] = null;
+        currentIndex = 0;
+        int greaterChildIndex = calculateGreaterChildIndex(currentIndex);
+        E greaterChildElement = maxHeapArray[greaterChildIndex];
+
+        while (currentElement.compareTo(greaterChildElement) == -1) {
+            maxHeapArray[currentIndex] = greaterChildElement;
+            maxHeapArray[greaterChildIndex] = currentElement;
+            currentIndex = greaterChildIndex;
+            currentElement = maxHeapArray[currentIndex];
+            greaterChildIndex = calculateGreaterChildIndex(currentIndex);
+            greaterChildElement = maxHeapArray[greaterChildIndex];
+            if (currentElement == null) {
+                break;
+            }
+        }
+        nextOpenIndex--;
+    }
+
+
+    private void percolateUp(Integer currentIndex) {
         Integer parentNodeIndex = calculateParentIndex(currentIndex);
         E parentElement = maxHeapArray[parentNodeIndex];
+        E currentElement = maxHeapArray[currentIndex];
 
-        while (parentElement.compareTo(element) == -1) {
-            maxHeapArray[parentNodeIndex] = element;
+        while (parentElement.compareTo(currentElement) == -1) {
+            maxHeapArray[parentNodeIndex] = currentElement;
             maxHeapArray[currentIndex] = parentElement;
             currentIndex = parentNodeIndex;
             parentNodeIndex = calculateParentIndex(currentIndex);
@@ -34,14 +72,49 @@ public class ArrayPriorityQueue<E extends Comparable<E>> {
         }
     }
 
+    private int calculateGreaterChildIndex(int parentIndex) {
+        E greaterChildElement;
+
+        int leftChildIndex = calculateLeftChildIndex(parentIndex);
+        E leftChildElement = maxHeapArray[leftChildIndex];
+
+        int rightChildIndex = calculateRightChildIndex(parentIndex);
+        E rightChildElement = maxHeapArray[rightChildIndex];
+
+        if (leftChildElement == null && rightChildElement != null) {
+            greaterChildElement = rightChildElement;
+        } else if (rightChildElement == null && leftChildElement != null) {
+            greaterChildElement = leftChildElement;
+        } else if (leftChildElement != null && leftChildElement.compareTo(rightChildElement) == 1) {
+            greaterChildElement = leftChildElement;
+        } else if (rightChildElement != null && rightChildElement.compareTo(leftChildElement) == 1) {
+            greaterChildElement = rightChildElement;
+        } else {
+            greaterChildElement = maxHeapArray[parentIndex];
+        }
+        return Arrays.asList(maxHeapArray).indexOf(greaterChildElement);
+    }
+
     private Integer calculateParentIndex(Integer currentIndex) {
-        Double intermediateParentNodeIndex = Math.floor((currentIndex-1)/2);
+        Double intermediateParentNodeIndex = Math.floor((currentIndex - 1) / 2);
         return intermediateParentNodeIndex.intValue();
     }
 
-    public int count() {
-        return maxHeapArray.length;
+    private int calculateLeftChildIndex(int parentIndex) {
+        return (parentIndex * 2) + 1;
     }
 
+    private int calculateRightChildIndex(int parentIndex) {
+        return (parentIndex * 2) + 2;
+    }
 
+    public int count() {
+        int totalElementCount = 0;
+        for (int currentIndex = 0; currentIndex < maxHeapArray.length; currentIndex++) {
+            if (maxHeapArray[currentIndex] != null) {
+                totalElementCount++;
+            }
+        }
+        return totalElementCount;
+    }
 }
--- src/challenge201_practical/ArrayPriorityQueueTest.java
+++ src/challenge201_practical/ArrayPriorityQueueTest.java
@@ -1,6 +1,9 @@
 package challenge201_practical;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by jon on 2/17/15.
@@ -14,5 +17,40 @@ public class ArrayPriorityQueueTest {
         priorityQueue = new ArrayPriorityQueue<>(Integer.class, 100);
     }
 
+
+    @Test
+    public void ensureOneElementCanBeAddedToPriorityQueue() {
+        priorityQueue.insert(1);
+        assertEquals(1, priorityQueue.count());
+    }
+
+    @Test
+    public void ensureMultipleElementsCanBeProperlyAddedToPriorityQueue() {
+        priorityQueue.insert(4);
+        priorityQueue.insert(2);
+        priorityQueue.insert(9);
+        priorityQueue.insert(0);
+        priorityQueue.insert(15);
+
+        Integer greatestElement = priorityQueue.dequeue();
+        assertEquals(new Integer(15), greatestElement);
+
+        Integer secondGreatestElement = priorityQueue.dequeue();
+        assertEquals(new Integer(9), secondGreatestElement);
+
+        Integer thirdGreatestElement = priorityQueue.dequeue();
+        assertEquals(new Integer(4), thirdGreatestElement);
+
+        Integer fourthGreatestElement = priorityQueue.dequeue();
+        assertEquals(new Integer(2), fourthGreatestElement);
+
+        Integer lastElement = priorityQueue.dequeue();
+        assertEquals(new Integer(0), lastElement);
+    }
+
+    @Test(expected = PriorityQueueEmptyException.class)
+    public void ensurePriorityQueueThrowsExceptionWhenEmptyAndDequeued() {
+        priorityQueue.dequeue();
+    }
 
 }
